35911be1db86d6ef80826e1be84c4a8d85176a6c
Federico Masi <federico.masi.k333@gmail.com>
2026-02-20 10:14:53 +0100

    Added nvim-tree.

---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+
+    vim.keymap.set("n", "<Leader>b", ":NvimTreeToggle<CR>", { desc = "Open NvimTree" })
+  end,
+}
+
