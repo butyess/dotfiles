580fb85f8cb5e4e39b64b0bc0f29f09563ab17c2
Federico Masi <federico.masi.k333@gmail.com>
2026-02-26 11:10:31 +0100

    Replace C-Space with C-l for completion trigger and add C-e to close

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -42,7 +42,8 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-l>'] = cmp.mapping.complete(),
         ['<Tab>'] = function(fallback)
           if require('luasnip').expandable() then
             require('luasnip').expand()
