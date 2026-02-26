0f09d0890b80fb9cd2b7471c4759bb89eb5b399d
Federico Masi <federico.masi.k333@gmail.com>
2026-02-26 10:29:41 +0100

    Add LuaSnip with ifmain snippet and Tab keybindings

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -22,6 +22,7 @@ return {
 
     local cmp = require('cmp')
     local cmp_lsp = require('cmp_nvim_lsp')
+    require('luasnip.loaders.from_lua').lazy_load({ paths = { vim.fn.stdpath('config') .. '/lua/snippets' } })
 
     -- Broadcast enhanced capabilities to all LSP servers
     vim.lsp.config('*', {
@@ -42,6 +43,20 @@ return {
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Tab>'] = function(fallback)
+          if require('luasnip').expandable() then
+            require('luasnip').expand()
+          else
+            fallback()
+          end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if require('luasnip').jumpable(-1) then
+            require('luasnip').jump(-1)
+          else
+            fallback()
+          end
+        end,
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
---
+++ nvim/.config/nvim/lua/snippets/python.lua
@@ -0,0 +1,12 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+  s("ifmain", {
+    t('if __name__ == "__main__":'),
+    t({ "", "    " }),
+    i(0),
+  }),
+}
