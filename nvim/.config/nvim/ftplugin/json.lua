3f383283e309d1fdeb9bb50bd4da201ba1e371d6
Federico Masi <federico.masi.k333@gmail.com>
2026-03-13 11:48:31 +0100

    json indent 2

--- nvim/.config/nvim/ftplugin/json.lua
+++ nvim/.config/nvim/ftplugin/json.lua
@@ -3,3 +3,7 @@ vim.treesitter.start()
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo[0][0].foldmethod = 'expr'
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
